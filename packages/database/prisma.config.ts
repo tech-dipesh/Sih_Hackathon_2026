c8b3053f84b6a50e862257745df50f4ef8d99a1f
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-09-08 15:57:33 +0530

    Add the tsconfig setup for the root files, with also add a prisma config setup and a api package for the backend setup

---
+++ packages/database/prisma.config.ts
@@ -0,0 +1,12 @@
+import { config } "dotenv";
+import { defineConfig, env } from "prisma/config";
+config({env: "../../"})
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
